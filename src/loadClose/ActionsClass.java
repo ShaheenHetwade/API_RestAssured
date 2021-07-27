66d17cd753909b42289b19bf9c00cfb1dfed0180
ShaheenHetwade <87909125+ShaheenHetwade@users.noreply.github.com>
2021-07-27 17:21:56 +0530

    Update ActionsClass.java

--- src/loadClose/ActionsClass.java
+++ src/loadClose/ActionsClass.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+//Actions using Actions class
 public class ActionsClass {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
